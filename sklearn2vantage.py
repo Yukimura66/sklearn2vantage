6eee1c7b220f2894a03b0e99de6381670d2a1bc6
Akihiro Sanada <akihiro.sanada@icloud.com>
2020-02-02 11:46:54 +0900

    RandomForest

--- sklearn2vantage.py
+++ sklearn2vantage.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from sqlalchemy import Column, Integer, Float, String
 import itertools
+import sklearn.tree
+from sqlalchemy_teradata.types import CLOB
 
 
 def make_model_table_glm(model, feature_names=None, isLogistic=False,
@@ -77,75 +79,68 @@ def load_model_nb(df_model, engine, table_name):
                     dtype=dtype_glm_model)
 
 
-def make_model_table_tree(model, feature_names):
+def make_model_table_tree(model, feature_names, isRegression=False):
+    id_trees = calcIdTrees(model)
+    n_node = len(model.tree_.children_left)
     is_split_node = model.tree_.children_left != -1
-    n_split_node = is_split_node.sum()
-    null_col = np.repeat(None, n_split_node)
-    blank_col = np.repeat("", n_split_node)
+    n_split_node = sum(is_split_node)
     children_left = model.tree_.children_left[is_split_node]
     children_right = model.tree_.children_right[is_split_node]
-    def func(x): return model.classes_[np.argmax(x)]
-    votes = model.tree_.value.squeeze()
-    labels = np.apply_along_axis(func, 1, votes)
-    majorvotes = votes.max(axis=1)
     size = model.tree_.n_node_samples
     features = feature_names[model.tree_.feature]
-    probs = votes/votes.sum(axis=1).reshape(-1, 1)
 
-    tree_table = pd.DataFrame({"node_id": np.where(is_split_node)[0],
-                               "node_size": size[is_split_node],
-                               "node_gini": (model.tree_
-                                                  .impurity[is_split_node]),
-                               "node_entropy": null_col,
-                               "node_chisq_pv": null_col,
-                               "node_label": labels[is_split_node],
-                               "node_majorvotes": majorvotes[is_split_node],
-                               "split_value": (model.tree_
-                                                    .threshold[is_split_node]),
-                               "split_gini": null_col,
-                               "split_entropy": null_col,
-                               "split_chisq_pv": null_col,
-                               "left_id": children_left,
-                               "left_size": size[children_left],
-                               "left_label": labels[children_left],
-                               "left_majorvotes": majorvotes[children_left],
-                               "right_id": children_right,
-                               "right_size": size[children_right],
-                               "right_label": labels[children_right],
-                               "right_majorvotes": null_col,
-                               "left_bucket": blank_col,
-                               "right_bucket": blank_col,
-                               "left_label_probdist":
-                                   [",".join([f"{val:.5f}" for val in val])
-                                       for val in probs[children_left]],
-                               "right_label_probdist":
-                                   [",".join([f"{val:.5f}" for val in val])
-                                       for val in probs[children_right]],
-                               "prob_label_order":
-                                   [",".join(list(model.classes_.astype(str)))]
-                               * n_split_node,
-                               "attribute": list(features[is_split_node])})
+    if isRegression:
+        labels = np.array(
+            [f"{val:.10f}" for val in model.tree_.value.squeeze()])
+        majorvotes = probs = np.repeat(None, n_node)
+        left_label_prob = right_label_prob = prob_label_order \
+            = [None] * n_split_node
+    else:
+        votes = model.tree_.value.squeeze()
+        labels = np.apply_along_axis(lambda x: model.classes_[np.argmax(x)],
+                                     1, votes)
+        majorvotes = votes.max(axis=1)
+        probs = votes/votes.sum(axis=1).reshape(-1, 1)
+        left_label_prob = [",".join([f"{val:.5f}" for val in val])
+                           for val in probs[children_left]]
+        right_label_prob = [",".join([f"{val:.5f}" for val in val])
+                            for val in probs[children_right]]
+        prob_label_order = [",".join(list(model.classes_.astype(str)))] \
+            * n_split_node
 
-    split_nodes = list(np.where(is_split_node)[0])
-    children_left_new = children_left.copy()
-    children_right_new = children_right.copy()
-    replace_dict = {}
-
-    for i in range(len(split_nodes)):
-        split_node = split_nodes.pop(0)
-        replace_dict[children_left[i]] = split_node * 2 + 1
-        replace_dict[children_right[i]] = split_node * 2 + 2
-        split_nodes = [replace_dict[val] if val in replace_dict else val
-                       for val in split_nodes]
-
-    tree_table[["node_id", "left_id", "right_id"]] = \
-        tree_table[["node_id", "left_id", "right_id"]].replace(replace_dict)
+    tree_table = pd.DataFrame(
+        {"node_id": id_trees[is_split_node],
+         "node_size": size[is_split_node],
+         "node_gini": model.tree_.impurity[is_split_node],
+         "node_entropy": np.repeat(None, n_split_node),
+         "node_chisq_pv": np.repeat(None, n_split_node),
+         "node_label": labels[is_split_node],
+         "node_majorvotes": majorvotes[is_split_node],
+         "split_value": model.tree_.threshold[is_split_node],
+         "split_gini": np.repeat(None, n_split_node),
+         "split_entropy": np.repeat(None, n_split_node),
+         "split_chisq_pv": np.repeat(None, n_split_node),
+         "left_id": id_trees[children_left],
+         "left_size": size[children_left],
+         "left_label": labels[children_left],
+         "left_majorvotes": majorvotes[children_left],
+         "right_id": id_trees[children_right],
+         "right_size": size[children_right],
+         "right_label": labels[children_right],
+         "right_majorvotes": np.repeat(None, n_split_node),
+         "left_bucket": np.repeat("", n_split_node),
+         "right_bucket": np.repeat("", n_split_node),
+         "left_label_probdist": left_label_prob,
+         "right_label_probdist": right_label_prob,
+         "prob_label_order": prob_label_order,
+         "attribute": list(features[is_split_node])})
 
     return tree_table
 
 
 def load_model_tree(df_model, engine, table_name):
-    def max_len(x): return df_model[x].str.len().max()
+    def max_len(x): 
+        return int(np.nan_to_num(df_model[x].str.len().max(), nan=1))
     dtype_tree_model = {"node_id": Integer, "node_size": Integer,
                         "node_gini": Float, "node_entropy": Float,
                         "node_chisq_pv": Float,
@@ -167,6 +162,147 @@ def load_model_tree(df_model, engine, table_name):
                         "prob_label_order":
                             String(length=max_len("prob_label_order")),
                         "attribute": String(length=max_len("attribute"))}
+    df_model.to_sql(name=table_name, con=engine,
+                    index=False, if_exists="replace",
+                    dtype=dtype_tree_model)
+
+
+def dict2json(obj: dict) -> str:
+    json_str = (str(obj).replace("\\", "")
+                .replace("\'", '\"').replace('"{', "{")
+                .replace('}"', "}"))
+    return json_str
+
+
+def makeTreeJson(tree: sklearn.tree._tree.Tree, idx_node: int,
+                 features: list, isRegression: bool = False,
+                 classes: list = None, depth: int = 0) -> str:
+    if isRegression and tree.criterion != "mse":
+        raise ValueError("criterion must be 'mse'")
+
+    id_trees = calcIdTrees(tree)
+    type_str = "REGRESSION" if isRegression else "CLASSIFICATION"
+
+    tree_dict = {"size_": tree.tree_.n_node_samples[idx_node],
+                 "id_": id_trees[idx_node]}
+    if isRegression:
+        tree_dict.update({
+            "sum_": tree.tree_.value.squeeze()[idx_node]
+            * tree.tree_.n_node_samples[idx_node],
+            "sumSq_": tree.tree_.impurity[idx_node]
+            * tree.tree_.n_node_samples[idx_node]
+        })
+
+    if tree.tree_.children_left[idx_node] != -1:  # split node
+        tree_dict.update(
+            {"maxDepth_": tree.tree_.max_depth - depth,
+             "split_": {
+                 "splitValue_": tree.tree_.threshold[idx_node],
+                 "attr_": features[tree.tree_.feature[idx_node]],
+                 "score_": tree.tree_.impurity[idx_node],
+                 "type_": f"{type_str}_NUMERIC_SPLIT",
+                 "leftNodeSize_":
+                     tree.tree_.n_node_samples[
+                     tree.tree_.children_left[idx_node]
+                 ],
+                 "rightNodeSize_":
+                     tree.tree_.n_node_samples[
+                     tree.tree_.children_right[idx_node]
+                 ],
+                 "scoreImprove_": calcScoreImprove(tree, idx_node)},
+             "leftChild_": makeTreeJson(
+                 tree, tree.tree_.children_left[idx_node], features,
+                 isRegression, classes, depth+1),
+             "rightChild_": makeTreeJson(
+                 tree, tree.tree_.children_right[idx_node], features,
+                 isRegression, classes, depth+1),
+             "nodeType_": f"{type_str}_NODE"
+             }
+        )
+        if not isRegression:
+            tree_dict.update({
+                "responseCounts_":
+                    {str(class_name): int(n_class_samp)
+                        for class_name, n_class_samp
+                        in zip(classes, tree.tree_.value[idx_node].squeeze())}
+            })
+
+    else:  # leaf node
+        tree_dict.update(
+            {"maxDepth_": 0,
+             "nodeType_": f"{type_str}_LEAF"}
+        )
+        if isRegression:
+            tree_dict.update({"value_": tree.tree_.value.squeeze()[idx_node]})
+        else:
+            tree_dict.update({
+                "label_": str(classes[
+                    int(tree.classes_[
+                        tree.tree_.value[idx_node].argmax()])
+                ])
+            })
+
+    return dict2json(tree_dict)
+
+
+def getNodeDict(tree: sklearn.tree._tree.Tree,
+                idx_node: int, new_idx_node: int) -> dict:
+    nodeDict = {}
+    left_idx_node = tree.tree_.children_left[idx_node]
+    right_idx_node = tree.tree_.children_right[idx_node]
+    nodeDict[left_idx_node] = new_idx_node * 2 + 1
+    nodeDict[right_idx_node] = new_idx_node * 2 + 2
+    split_nodes = np.where(tree.tree_.children_left != -1)[0]
+    if left_idx_node in split_nodes:
+        nodeDict.update(
+            getNodeDict(tree, left_idx_node, new_idx_node * 2 + 1)
+        )
+    if right_idx_node in split_nodes:
+        nodeDict.update(
+            getNodeDict(tree, right_idx_node, new_idx_node * 2 + 2)
+        )
+    return nodeDict
+
+
+def calcIdTrees(tree: sklearn.tree._tree.Tree) -> np.ndarray:
+    nodeDict = getNodeDict(tree, 0, 0)
+    nodeDict.update({0: 0})
+    nodeDict = sorted(nodeDict.items())
+    return np.array([x[1] for x in nodeDict])
+
+
+def calcScoreImprove(tree, idx_node) -> float:
+    n_sample = tree.tree_.n_node_samples[idx_node]
+    idx_left = tree.tree_.children_left[idx_node]
+    idx_right = tree.tree_.children_right[idx_node]
+    n_sample_left = tree.tree_.n_node_samples[idx_left]
+    n_sample_right = tree.tree_.n_node_samples[idx_right]
+    impurity_left = tree.tree_.impurity[idx_left]
+    impurity_right = tree.tree_.impurity[idx_right]
+    impurity_after = (n_sample_left / n_sample * impurity_left
+                      + n_sample_right / n_sample * impurity_right)
+    return tree.tree_.impurity[idx_node] - impurity_after
+
+
+def make_model_table_forest(model, feature_names, classes: list = None,
+                            isRegression=False):
+    forest_table = pd.DataFrame(
+        {"worker_ip": ["100.0.0.0"] * model.n_estimators,
+         "task_index": [0] * model.n_estimators,
+         "tree_num": np.arange(model.n_estimators),
+         "tree": [makeTreeJson(a_tree, 0, feature_names, isRegression, classes)
+                  for a_tree in model.estimators_]
+         }
+    )
+    return forest_table
+
+
+def load_model_forest(df_model, engine, table_name):
+    def max_len(x): return df_model[x].str.len().max()
+    dtype_tree_model = {"worker_ip": String(max_len("worker_ip")),
+                        "task_index": Integer,
+                        "tree_num": Integer,
+                        "tree": CLOB}
     df_model.to_sql(name=table_name, con=engine,
                     index=False, if_exists="replace",
                     dtype=dtype_tree_model)
