d449ce82288525437127e845e27be081843d9ac3
Akihiro Sanada <akihiro.sanada@icloud.com>
2020-02-22 12:35:58 +0900

    add README to long_description in setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,15 @@ requires = ["numpy", "pandas", "SQLAlchemy", "scikit-learn", "teradata",
             "sqlalchemy-teradata", "teradatasql", "teradatasqlalchemy",
             "paramiko", "scp"]
 
+with open('README.rst', encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
 setup(
     name="sklearn2vantage",
-    version="0.1.5",
+    version="0.1.6",
     description="Module for converting sklearn model to Teradata Vantage"
                 + " model",
+    long_description=readme,
     url="https://github.com/Yukimura66/sklearn2vantage",
     author="Akihiro Sanada",
     author_email="akihiro.sanada@icloud.com",
